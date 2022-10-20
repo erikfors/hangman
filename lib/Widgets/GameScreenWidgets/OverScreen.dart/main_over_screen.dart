3c1643b349da4a8a250b5d953a4113dbb8303a42
erikfors <efors003@fiu.edu>
2022-10-20 19:56:59 -0400

    Added fetcher for word definitions

--- lib/Widgets/GameScreenWidgets/OverScreen.dart/main_over_screen.dart
+++ lib/Widgets/GameScreenWidgets/OverScreen.dart/main_over_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hangman/Providers/GameHangman/bloc/game_hangman_bloc.dart';
+import 'package:hangman/Providers/WordDefinitionProvider/bloc/word_definition_bloc.dart';
 
 class MainOverScreen extends StatelessWidget {
   final GameHangmanState gameState;
@@ -7,59 +9,79 @@ class MainOverScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(
-          width: double.infinity,
-          child: Text(
-            "Total Score: ${gameState.totalScore}",
-            textAlign: TextAlign.end,
+    return BlocProvider(
+      create: (context) => WordDefinitionBloc(gameState.word)..add(WordDefinitionFetched(),),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: double.infinity,
+            child: Text(
+              "Total Score: ${gameState.totalScore}",
+              textAlign: TextAlign.end,
+            ),
           ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Text(
-          gameState.gameWon ? "You Win" : "You Lost",
-          style: Theme.of(context).textTheme.headlineLarge,
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Text(
-          gameState.word,
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Text(
-          "A Word deffinition",
-          style: Theme.of(context).textTheme.bodyLarge,
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        !gameState.gameWon
-            ? ElevatedButton(
-                onPressed: () {},
-                child: const Text("Try Again"),
-              )
-            : ElevatedButton(
-                onPressed: () {},
-                child: const Text("Next Word!"),
-              ),
-        const SizedBox(
-          height: 10,
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          child: Text(
-            !gameState.gameWon ? "Exit" : "Save and Exit",
+          const SizedBox(
+            height: 10,
           ),
-        ),
-      ],
+          Text(
+            gameState.gameWon ? "You Win" : "You Lost",
+            style: Theme.of(context).textTheme.headlineLarge,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            gameState.word,
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          BlocBuilder<WordDefinitionBloc, WordDefinitionState>(
+            builder: (context, state) {
+              switch (state.status) {
+                case WordDefinitionStatus.initial:
+                  return Text(
+                    "Getting your definition...",
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  );
+                case WordDefinitionStatus.success:
+                  return Text(
+                    state.definitions[0],
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  );
+                case WordDefinitionStatus.failure:
+                  return Text(
+                    "Could not get definition :(",
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  );
+              }
+            },
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          !gameState.gameWon
+              ? ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("Try Again"),
+                )
+              : ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("Next Word!"),
+                ),
+          const SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              !gameState.gameWon ? "Exit" : "Save and Exit",
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
